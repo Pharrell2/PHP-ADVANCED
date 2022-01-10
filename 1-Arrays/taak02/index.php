cb839bc387574c26accba1376b1f1efedf3dd90b
Pharrell Helstone <2153880@talnet.nl>
2022-01-10 11:00:04 +0100

    taak 2 af

---
+++ 1-Arrays/taak02/index.php
@@ -0,0 +1,9 @@
+<?php
+$persoon["voornaam"]   = "Pharrell";
+$persoon["achternaam"] = "Helstone";
+$persoon["leeftijd"]       = 16;
+$persoon["opleiding"]   = "software developing";
+
+echo "Mijn naam is" . " " . $persoon['voornaam'] . " " . $persoon['achternaam'] . " " . ", ik ben" . " " .  $persoon['leeftijd'] . " " . "jaar oud". " " . "Ik zit op het ROC van amsterdam en ik doe de opleiding" . " " . $persoon['opleiding']; 
+
+?>
