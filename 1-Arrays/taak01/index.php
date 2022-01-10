2499f992e4da1529644b77232bb1778733651573
Pharrell Helstone <2153880@talnet.nl>
2022-01-10 10:32:13 +0100

    taak 1 af

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,8 @@
+<?php
+$student['voornaam'] = "Pharrell";
+$student['achternaam'] = "Helstone";
+$student['leeftijd'] = 16;
+$student['klas'] = '1D';
+
+echo "Gegevens Pharrell: Voornaam:". " " . " " . $student['voornaam'] . " " . "Achternaam:" . $student['achternaam'] . " " . "Leeftijd:" . " " . $student['leeftijd'] . " ". "Klas:" . " " . $student['klas'];
+?> 
