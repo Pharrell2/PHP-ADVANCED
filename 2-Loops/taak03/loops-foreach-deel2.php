cdcc28d59b458410010111c37554f8144c4d99d2
Pharrell Helstone <2153880@talnet.nl>
2022-01-13 14:54:35 +0100

    taak 3 af

--- 2-Loops/taak03/loops-foreach-deel2.php
+++ 2-Loops/taak03/loops-foreach-deel2.php
@@ -1,24 +1,36 @@
-<?php
-echo "Jij komt uit klas: " . $_GET['school'];
-?> 
-  6  2-Loops/taak04/loops-foreach-deel2.php 
-@@ -8,10 +8,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <linkrel="stylesheet" href="taak4.css">
     <title>Document</title>
 </head>
 <body>
-<form class= "Form">
-
-<form class= "Form" method="GET" action="JEBEM.php">
-    <label>Klassenformulier</label> <br>
-    <label>Ik zit in klas:</label> 
-<select>
-<select id="school" name="school">
+<form class= "Form" method="get" action="LF2.php">
+<label>Klassenformulier</label><br>
+<label>Ik zit in klas:</label>
+<selectid="naam" name="naam">
+    
 <?php
-$Klas = "8B";
-$klassen = ['8A' , '8B' , '8C' , '8D' , '8E' , '9A' , '9B' , '9C' , '9D' , '9E'];
-</select> <br>
-<button class= "btn">Verzend</button>
-</form>
+$klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
+foreach ($klassen as $klas){
+    if($Klas = $klas){
+        echo"<option value='$Klas' selected>$klas</option>";
+    }
+    else{
+        echo"<option value='$Klas'>$klas</option>";
+    }
+}
 
+?>
+</select><br>
+
+<button class="button">Verzend</button>
+</form>
 </body>
 </html>
+
+
+
