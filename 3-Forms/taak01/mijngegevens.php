df6692db2460e032630bc05149a64a64c060b122
Pharrell Helstone <2153880@talnet.nl>
2022-01-14 00:52:59 +0100

    taak 1 af

---
+++ 3-Forms/taak01/mijngegevens.php
@@ -0,0 +1,6 @@
+<?php
+$student  = array("voornaam"    => "Piet","achternaam"  => "Pietersen", "klas"        => "9G","Leeftijd"    => 18, "Adres"  => "Kalverstraat 11","Woonplaats" => "Amsterdam");
+  foreach ($student as $student){
+      echo "$student<br>";
+  }
+?>
