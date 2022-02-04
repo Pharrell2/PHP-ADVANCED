00f1c642f667f1269335323b88fcb1ed2c4e05ad
Pharrell Helstone <2153880@talnet.nl>
2022-02-05 00:54:10 +0100

    taak 3 af

---
+++ 3-Forms/taak03/mijnnummer.php
@@ -0,0 +1,6 @@
+<?php     
+foreach($_GET as $key=> $value){
+echo $value . "<br>";
+}
+http://localhost/PHP-ADVANCED/3-Forms/taak03/mijnnummer.php?Artiest=Michael+Jackson&titel=Smooth+criminal&jaar=2001&muzieksoort=pop%2Fr%26b&submit=verstuur
+?>
