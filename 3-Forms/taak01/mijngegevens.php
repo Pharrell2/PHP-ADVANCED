bd27e4e43329e25f570ab50d9712a45dd758e768
Pharrell Helstone <2153880@talnet.nl>
2022-03-01 22:56:11 +0100

    taak 1 verbeterd

--- 3-Forms/taak01/mijngegevens.php
+++ 3-Forms/taak01/mijngegevens.php
@@ -1,6 +1,4 @@
-
 <?php
-echo "Pharrell Helstone 1D Amsterdam Nederland:". "<br>" . $_GET['voornaam'] . "<br>";
 echo $_GET ['voornaam'] . "<br>";
 echo $_GET ['achternaam'] . "<br>"; 
 echo $_GET ['klas'] . "<br>";
